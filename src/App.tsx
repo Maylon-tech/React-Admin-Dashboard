7a062d9aff4daa2661643cbc7c37f9daee6f6fae
Maylon-tech <minatokiroisen77@gmail.com>
2023-09-20 08:25:54 +0900

    preparing the app and setting up

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import './App.css'
+import Footer from './components/footer/Footer'
+import Menu from './components/menu/Menu'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Products from './pages/product/Products'
+import Users from './pages/users/Users'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from 'react-router-dom'
+
+
+function App() {
+
+  const Layout = () => {
+    return (
+      <div className='main'>
+        <Navbar />
+        <div className='container'>
+          <div className='menuContainer'>
+            <Menu />
+          </div>
+          <div className='contentContainer'>
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "users",
+          element: <Users />
+        },
+        {
+          path: "products",
+          element: <Products />
+        }
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ])
+
+
+  return (
+    <RouterProvider router={router}>
+      
+    </RouterProvider>
+  )
+}
+
+export default App
---
+++ src/pages/product/Products.tsx
@@ -0,0 +1,9 @@
+import './product.scss'
+
+const Products = () => {
+  return (
+    <div className="product">Products</div>
+  )
+}
+
+export default Products
